c02ab1e12e3bc2e9ec5303635f441fb72221b2dd
Andree Monette <andreecmonette@gmail.com>
2014-04-02 16:57:41 -0400

    added models

--- models.py
+++ models.py
@@ -1,2 +1,17 @@
-from sqlalchemy import create_engine
-engine = create_engine('postgres:///:memory:', echo=True)
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from main import mud_app
+
+engine = create_engine(mud_app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+
+Base = declarative_base()
+
+class Player(Base):
+  __tablename__ = 'players'
+
+  id = Column(Integer, primary_key=True)
+  public_name = Column(String)
+  login = Column(String)
+  password = Column(String)
+  experience = Column(Integer)
+
