d13508be20fe4ab0385229595ad1384ec656cdcf
Chen Lin <clin_@berkeley.edu>
2014-04-02 15:48:43 -0400

    Temporarily removing 404.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ mud_app = Flask(__name__)
 @mud_app.route('/mud')
 def hello_world():
   return 'Hello World!'
-
-@mud_app.errorhandler(404)
-def page_not_found(e):
-  return 'You didn\'t want to go here. Go to <a href=\'/mud\'>this page</a> instead.', 404
-
+#
+# @mud_app.errorhandler(404)
+# def page_not_found(e):
+#   return 'You didn\'t want to go here. Go to <a href=\'/mud\'>this page</a> instead.', 404
+#
