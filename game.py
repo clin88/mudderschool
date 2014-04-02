333d1709cbde0f81614c85def120fffbfb5ebbd0
Andree Monette <andreecmonette@gmail.com>
2014-04-02 16:31:09 -0400

    db boilerplate

---
+++ game.py
@@ -0,0 +1,10 @@
+"""
+Basic resources: 
+Prestige
+You have four 'cores' (equip slots)
+
+Form a subnet with someone
+"""
+
+
+class Player(object): 
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+""" You are a hacker, looking to delve into the world's megacorporations for
+fun and profit.  One day, a friend contacts you looking to gain info about IntroSys Inc.
+From there, your journey is up to you."""
+
 from flask import Flask
+from flask.ext.heroku import Heroku
+
 mud_app = Flask(__name__)
+heroku = Heroku(mud_app)
 
 @mud_app.route('/mud')
 def hello_world():
