29cf0a30984aa499eeaef3fe5a58a846ef97381a
Chen Lin <clin_@berkeley.edu>
2014-04-03 11:56:05 -0400

    Fixing 'g'.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 fun and profit.  One day, a friend contacts you looking to gain info about IntroSys Inc.
 From there, your journey is up to you."""
 
-from flask import Flask, g
+from flask import Flask, g, render_template
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
+
+# WARNING: TURN OFF IF PRODUCTION
+# app.debug = True
 heroku = Heroku(app)
 
 @app.route('/mud')
@@ -18,5 +21,5 @@ def page_not_found(e):
 
 @app.route('/debug')
 def debug():
-  g['yo'] = 'sup'
+  g.yo = 'sup'
   return render_template('debug.html')
